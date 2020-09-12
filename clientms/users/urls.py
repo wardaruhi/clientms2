a31a359d6b4fa2e0002f04bb2b88913871a927b0
wardaruhi <60083036+wardaruhi@users.noreply.github.com>
2020-09-12 18:32:53 -0500

    Extra Credit Feature 1 Changepass

--- clientms/users/urls.py
+++ clientms/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 #from . import views
-from .views import SignUpView
+from .views import SignUpView, ChangePasswordResetDoneSuccessView, ChangePasswordResetDoneView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('password_change/', ChangePasswordResetDoneView.as_view(), name='password_change'),
+    path('password_changedone', ChangePasswordResetDoneSuccessView.as_view(), name = 'password_changedone'),
 ]
--- clientms/users/views.py
+++ clientms/users/views.py
@@ -3,8 +3,19 @@ from django.views import generic
 
 from .forms import CustomUserCreationForm
 
+from django.contrib.auth import views as auth_views, forms as auth_forms
+
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+class ChangePasswordResetDoneView(auth_views.PasswordChangeView):
+    form_class = auth_forms.PasswordChangeForm
+    template_name = 'password_change.html'
+    success_url = reverse_lazy('password_changedone')
+
+class ChangePasswordResetDoneSuccessView(auth_views.PasswordChangeView):
+    form_class = auth_forms.PasswordChangeForm
+    template_name = 'password_changedone.html'
