346135949668f67ee7b5b80c4688789c62c406d1
wardaruhi <60083036+wardaruhi@users.noreply.github.com>
2020-09-12 16:30:54 -0500

    Initial commit

---
+++ clientms/clients/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from .views import (
+    ClientListView,
+    ClientUpdateView,
+    ClientDetailView,
+    ClientDeleteView,
+    ClientCreateView
+)
+
+urlpatterns = [
+    path('<int:pk>/edit/',
+         ClientUpdateView.as_view(), name='client_edit'),
+    path('<int:pk>/',
+         ClientDetailView.as_view(), name='client_detail'),
+    path('<int:pk>/delete/',
+         ClientDeleteView.as_view(), name='client_delete'),
+    path('new/', ClientCreateView.as_view(), name='client_new'),
+    path('', ClientListView.as_view(), name='client_list'),
+
+    ]
