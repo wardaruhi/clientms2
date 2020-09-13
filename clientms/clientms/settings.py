742e811594e10aa2cac1bdb66306618cb3e2c067
wardaruhi <60083036+wardaruhi@users.noreply.github.com>
2020-09-13 17:16:48 -0500

    Final Commit

--- clientms/clientms/settings.py
+++ clientms/clientms/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'qc!&gs(9$_2ch7dxgqf(sj1&za!o3^s3^#ydn+2dcz34t4tcd_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 AUTH_USER_MODEL = 'users.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
