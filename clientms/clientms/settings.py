d68a9f2663b3bc0c1463e8010ddecfffafc3389d
wardaruhi <60083036+wardaruhi@users.noreply.github.com>
2020-09-13 23:06:32 -0500

    Final Commit

--- clientms/clientms/settings.py
+++ clientms/clientms/settings.py
@@ -130,5 +130,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'indxpr@gmail.com'
-EMAIL_HOST_PASSWORD = 'AlphaBetaGamma123'
+EMAIL_HOST_PASSWORD = 'ChickenAteEggsAlphaBetaGamma123'
 EMAIL_USE_TLS = True
